53f2006bdf29472a7b3589ddc1fe9e506b7e4690
December G <dgarnetsmith@gmail.com>
2020-03-30 09:59:49 -0600

    initial commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const audit = {
 
     // What is the value of `this` when we call fn()?
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'global window object';
 
     return result;
   },
@@ -31,7 +31,7 @@ const audit = {
 
     // What is the value of `this` when we call data.verify())?
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'data';
 
     return result;
   },
@@ -40,7 +40,7 @@ const audit = {
 
 
 
-  
+
 
 
   exerciseC() {
@@ -54,7 +54,7 @@ const audit = {
 
     // What is the value of `this` when we call dog.bark();
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'dog';
 
     return result;
   },
@@ -92,9 +92,12 @@ const audit = {
     // Log D: greeting
     greetingGenerator();
 
-    const result = [{
-      // 'ADD YOUR RESULT HERE';
-    }];
+    const result = [
+      {'D': 'welcome'}
+      {'A': 'hey'}
+      {'B': 'hi'}
+      {'C': 'hey'}
+    ];
 
     return result;
   },
@@ -109,7 +112,7 @@ const audit = {
   exerciseE() {
 
     var shoe = 'heel';
-    // Log A: 
+    // Log A:
 
     function putOnShoe() {
       shoe = 'boot';
@@ -122,9 +125,12 @@ const audit = {
 
     // Log D: shoe
 
-    const result = [{
-      // 'ADD YOUR RESULT HERE';
-    }];
+    const result = [
+      {'A': 'heel'}
+      {'C': 'heel'}
+      {'B': 'boot'}
+      {'D': 'boot'}
+    ];
 
     return result;
   },
@@ -174,7 +180,8 @@ const audit = {
     // e.g.
     // ['Beckon', 'El Five', 'ChoLon', 'Super Mega Bien']
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+
+    const result = restaurants.map(restaurant => restaurant.name);
 
     return result;
   },
@@ -220,7 +227,7 @@ const audit = {
     ];
 
     // Return an array of objects that include just the name of the restaurant and whether takeout is available
-    
+
     // e.g.
     // [
     //    { restaurant: 'Beckon', takeOut: false },
@@ -228,7 +235,12 @@ const audit = {
     //    ..etc
     // ]
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    restaurants.map(restaurant => delete restaurant.capacity)
+    restaurants.map(restaurant => delete restaurant.cuisine)
+    restaurants.map(restaurant => delete restaurant.popular_dishes)
+
+
+    const result = restaurants;
 
     return result;
   },
@@ -282,14 +294,15 @@ const audit = {
     //    'Asian Fusion': ['ChoLon']
     // }
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result =  restaurants.map(restaurant => restaurant.cuisine = restaurant.name);
+
 
     return result;
   },
 
   exerciseBonus() {
 
-    // There is a war and nobody knows - the alphabet war! 
+    // There is a war and nobody knows - the alphabet war!
     // There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began.
 
     // Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return 'Left side wins!', when the right side wins return 'Right side wins!', in other case return 'Let's fight again!'.
@@ -329,7 +342,7 @@ const audit = {
       four: alphabetWar('wwwwwwz')
     };
 
-    return result;    
+    return result;
   }
 };
 
